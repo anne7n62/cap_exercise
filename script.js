b3c5ca580be283d050adc1db7626f6ce1b6d0202
Annemette <anne7n62@stud.kea.dk>
2021-03-02 12:27:24 +0100

    cap configurator works - done

--- script.js
+++ script.js
@@ -12,11 +12,36 @@ async function start() {
 function startManipulatingTheSvg() {
   console.log("manipulating");
   document.querySelectorAll(".g_to_interact_with").forEach((cap) => {
-    cap.addEventListener("click", colorChange);
+    cap.addEventListener("click", the_click);
+    cap.addEventListener("mouseover", the_mouseover);
+    cap.addEventListener("mouseout", the_mouseout);
+  });
+
+  document.querySelectorAll(".color_btn").forEach((btn) => {
+    btn.addEventListener("click", color_click);
   });
 }
 
-function colorChange(event) {
-  console.log("colorchange");
-  this.
+function the_click() {
+  elementToPaint = this;
+  this.style.fill = "grey";
+}
+
+function the_mouseover() {
+  console.log(this);
+
+  this.style.stroke = "blue";
+}
+
+function the_mouseout() {
+  console.log(this);
+
+  this.style.stroke = "none";
+}
+
+function color_click() {
+  console.log("KLIk", this.getAttribute("fill"));
+  if (elementToPaint != undefined) {
+    elementToPaint.style.fill = this.getAttribute("fill");
+  }
 }
